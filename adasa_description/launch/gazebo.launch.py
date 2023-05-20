1d7bce36f2509d610592dd380bd504540b79ab4a
Arpit Chauhan <carpit680@gmail.com>
2023-05-20 08:29:51 +0530

    odom funct but rotate tf cw 90z

--- adasa_description/launch/gazebo.launch.py
+++ adasa_description/launch/gazebo.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     rviz_config = rviz_file_name  # os.path.join(package_share, rviz_file_name)
 
     # World file
-    world_file_name = 'empty.world'
+    world_file_name = 'test.world'
     world_path = os.path.join(package_share, 'worlds', world_file_name)
 
     # Map file
@@ -126,10 +126,31 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}]
     )
 
+    pcl2lscn = Node(
+        package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+        remappings=[('cloud_in', ['/depth_camera/points']),
+                    ('scan', ['/scan'])],
+        parameters=[{
+                'target_frame': 'zed2_depth_frame',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 1.0,
+                'angle_min': -0.95993,  # -M_PI/2
+                'angle_max': 0.95993,  # M_PI/2
+                'angle_increment': 0.0027,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.2,
+                'range_max': 20.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+        }],
+        name='pointcloud_to_laserscan'
+    )
+
     return LaunchDescription([
-        declare_map_cmd,
+        # declare_map_cmd,
         declare_world_cmd,
-        map_server,
+        # map_server,
         declare_sim_time_cmd,
         gazebo_server,
         gazebo_client,
@@ -137,5 +158,6 @@ def generate_launch_description():
         joint_state_publisher,
         # joint_state_publisher_gui,
         spawn_entity,
-        rviz2
+        rviz2,
+        # pcl2lscn
     ])
